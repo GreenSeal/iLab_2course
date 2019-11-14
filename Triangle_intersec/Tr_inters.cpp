e833272f7ce15941ef29f708df9e9aab0ce46df6
GSTomashev <tomashev.dd@phystech.edu>
2019-11-15 01:35:30 +0300

    First non-optimised version

---
+++ Triangle_intersec/Tr_inters.cpp
@@ -0,0 +1,125 @@
+#include"Tr_inters.h"
+#include<fstream>
+
+template <typename T> Point_t<T> Find_lone_pnt(Plane_t<T> plane, Poligon_t<T> tr) {
+	int res_1 = Check_side(plane, tr.pt_vector[0], 1),
+	    res_2 = Check_side(plane, tr.pt_vector[1], 1),
+	    res_3 = Check_side(plane, tr.pt_vector[2], 1);
+
+	if(res_1 == res_2) return tr.pt_vector[2];
+	
+	else {
+		if(res_1 == res_3) return tr.pt_vector[1];
+		
+		else return tr.pt_vector[0];
+	}
+	
+}
+
+template <typename T> void Insert_tr(Poligon_t<T> * tr_vector, int num) {
+	
+	for(int i  = 0; i < num; i++) {
+		
+		Point_t<double> pnt_1, pnt_2, pnt_3;
+		std::cin >> pnt_1.x >> pnt_1.y >> pnt_1.z 
+			 >> pnt_2.x >> pnt_2.y >> pnt_2.z
+			 >> pnt_3.x >> pnt_3.y >> pnt_3.z;
+
+		Poligon_t<double> triangle(pnt_1, pnt_2, pnt_3);
+		
+		tr_vector[i] = triangle;
+	}
+}
+
+int main() {
+	int num;
+	std::cin >> num;
+
+	Poligon_t<double> * tr_vector = new Poligon_t<double>[num];
+
+	//Insert triangles
+	
+	Insert_tr(tr_vector, num);
+
+	for(int i = 0; i < num; i++) {
+
+		//Build plane on main triagnle
+		Plane_t<double> tr_plane(tr_vector[i].pt_vector[0], tr_vector[i].pt_vector[1], tr_vector[i].pt_vector[2]);
+
+		for(int j = 0; j < num; j++) {
+
+			if(j == i) continue;
+
+			//If point lies on different side of main plane, then triangles don't intersec
+			if(Check_side(tr_plane, tr_vector[j].pt_vector[0], 1) == Check_side(tr_plane, tr_vector[j].pt_vector[1], 1) 
+			&& Check_side(tr_plane, tr_vector[j].pt_vector[1], 1) == Check_side(tr_plane, tr_vector[j].pt_vector[2], 1)) {}
+
+			//Else they may be intersec
+			else {
+				//Find one point which lies alone on her side
+				Point_t<double> lonely_pnt = Find_lone_pnt(tr_plane, tr_vector[j]);
+				
+				std::vector<Line_t<double>> sides;
+
+				//Find sides which intersec main plane
+				for(int k = 0; k < 3; k++) {
+					Line_t<double> side(lonely_pnt, tr_vector[j].pt_vector[k]);
+
+					if(side.a_x == side.a_x || side.a_y == side.a_y || side.a_z == side.a_z) sides.push_back(side);
+				}
+
+				//Find points which are intersec sides and main plane
+				Point_t<double> pnt_segm_1, pnt_segm_2;
+				pnt_segm_1 = tr_plane.pnt_inters_line(sides[0]);
+				pnt_segm_2 = tr_plane.pnt_inters_line(sides[1]);
+
+				//Vector of normal to main plane
+				Point_t<double> norm_vec{tr_plane.A, tr_plane.B, tr_plane.C};
+
+				Point_t<double> pnt_dop_1{}, pnt_dop_2{}, pnt_dop_3{}, pnt_dop_segm{};
+				pnt_dop_1 = tr_vector[i].pt_vector[0] + norm_vec;
+				pnt_dop_2 = tr_vector[i].pt_vector[1] + norm_vec;
+				pnt_dop_3 = tr_vector[i].pt_vector[2] + norm_vec;
+				pnt_dop_segm = pnt_segm_1 + norm_vec;
+
+				//Build planes perpendicular sides and segment of intersec
+				Plane_t<double> side_plane_1{tr_vector[i].pt_vector[0], tr_vector[i].pt_vector[1], pnt_dop_1},
+						side_plane_2{tr_vector[i].pt_vector[1], tr_vector[i].pt_vector[2], pnt_dop_2},
+						side_plane_3{tr_vector[i].pt_vector[2], tr_vector[i].pt_vector[0], pnt_dop_3},
+						segm_plane{pnt_segm_1, pnt_segm_2, pnt_dop_segm};
+
+
+				std::vector<Plane_t<double>> side_planes = {side_plane_1, side_plane_2, side_plane_3};
+
+				//For more convenient sort out of points
+				std::vector<Point_t<double>> pnt_vector = {tr_vector[i].pt_vector[0], tr_vector[i].pt_vector[1], tr_vector[i].pt_vector[2], tr_vector[i].pt_vector[0]};
+
+				for(int k = 0; k < 3; k++) {
+					if(Check_side(side_planes[k], pnt_segm_1, 1) != Check_side(side_planes[k], pnt_segm_2, 1) 
+					&& Check_side(segm_plane, pnt_vector[k], 1) != Check_side(segm_plane, pnt_vector[k+1], 1)) {
+						
+						//std::cout << i << " inters with " << j << std::endl;
+						tr_vector[i].color = 1;
+						tr_vector[j].color = 1;
+					}
+				}
+			}
+
+		}
+	}
+
+	std::ofstream res;
+	res.open("tests/5.res");
+
+	for(int i = 0; i < num; i++) {
+		if(tr_vector[i].color == 1) res << i << std::endl;
+	}
+	
+	res.close();
+
+
+	delete [] tr_vector;
+
+
+	return 0;
+}
---
+++ Triangle_intersec/Tr_inters.h
@@ -0,0 +1,137 @@
+#include<iostream>
+#include<assert.h>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+
+double const tol = 0.000001;
+
+template <typename T> struct Point_t final {
+
+        T x, y, z;
+
+        explicit Point_t(T X = 0, T Y = 0, T Z = 0) : x(X), y(Y), z(Z) {};
+};
+
+template <typename T> struct Line_t final {
+
+        T a_x, a_y, a_z, x, y, z;
+
+        explicit Line_t(T A_x = 0, T A_y = 0, T A_z = 0, T X = 0, T Y = 0, T Z = 0) : a_x(A_x), a_y(A_y), a_z(A_z), x(X), y(Y), z(Z) {};
+
+	Line_t(Point_t<T> pnt_1, Point_t<T> pnt_2) : x(pnt_1.x), y(pnt_1.y), z(pnt_1.z) {
+		double vec_mod = sqrt((pnt_2.x-pnt_1.x)*(pnt_2.x-pnt_1.x) + (pnt_2.y-pnt_1.y)*(pnt_2.y-pnt_1.y) + (pnt_2.z-pnt_1.z)*(pnt_2.z-pnt_1.z));
+		a_x = (pnt_2.x - pnt_1.x)/vec_mod;
+		a_y = (pnt_2.y - pnt_1.y)/vec_mod;
+		a_z = (pnt_2.z - pnt_1.z)/vec_mod;
+	};
+};
+
+template <typename T> struct Plane_t final {
+	T A, B, C, D;
+
+	explicit Plane_t (T a = 0, T b = 0, T c = 0, T d = 0) : A(a), B(b), C(c), D(d) {};
+	
+	Plane_t(Point_t<T> pnt_1, Point_t<T> pnt_2, Point_t<T> pnt_3) {
+		
+		A = (pnt_2.y-pnt_1.y)*(pnt_3.z-pnt_1.z) - (pnt_3.y-pnt_1.y)*(pnt_2.z-pnt_1.z),
+		B = (pnt_2.z-pnt_1.z)*(pnt_3.x-pnt_1.x) - (pnt_2.x-pnt_1.x)*(pnt_3.z-pnt_1.z),
+		C = (pnt_2.x-pnt_1.x)*(pnt_3.y-pnt_1.y) - (pnt_2.y-pnt_1.y)*(pnt_3.x-pnt_1.x),
+		D = pnt_1.x*((pnt_3.y-pnt_1.y)*(pnt_2.z-pnt_1.z) - (pnt_2.y-pnt_1.y)*(pnt_3.z-pnt_1.z)) + 
+		    pnt_1.y*((pnt_2.x-pnt_1.x)*(pnt_3.z-pnt_1.z) - (pnt_2.z-pnt_1.z)*(pnt_3.x-pnt_1.x)) + 
+		    pnt_1.z*((pnt_2.y-pnt_1.y)*(pnt_3.x-pnt_1.x) - (pnt_2.x-pnt_1.x)*(pnt_3.y-pnt_1.y));
+	};
+
+	double dist_point(Point_t<T> pnt) {
+		double dist;
+		dist = (A*pnt.x + B*pnt.y + C*pnt.z + D)/sqrt(A*A + B*B + C*C);
+		if(dist < 0) dist = -dist;
+		return dist;
+	};
+
+	double angle_line(Line_t<T> line) {
+		double sin_angle;
+		sin_angle = (line.a_x*A + line.a_y*B + line.a_z*C)/(sqrt(A*A + B*B + C*C) * sqrt(line.a_x*line.a_x + line.a_y*line.a_y + line.a_z*line.a_z));
+		if(sin_angle < 0) sin_angle = -sin_angle;
+		return sin_angle;
+	};
+
+	Point_t<T> pnt_inters_line(Line_t<T> line) {
+		double sin_a = angle_line(line);
+		Point_t<T> line_pnt{line.x, line.y, line.z};
+		double dist = dist_point(line_pnt);
+		double lengh = dist/sin_a;
+		Point_t<T> diff{line.a_x*lengh, line.a_y*lengh, line.a_z*lengh};
+		Point_t<T> pnt_inters{line_pnt.x + diff.x, line_pnt.y + diff.y, line.z + diff.z};
+		return pnt_inters;
+	};
+};
+
+template <typename T> struct Poligon_t final {
+
+        std::vector<Point_t<T>> pt_vector;
+	int color;
+
+	explicit Poligon_t(int col = 0) : pt_vector(), color(col) {};
+
+        Poligon_t(Point_t<T> pnt_1, Point_t<T> pnt_2, Point_t<T> pnt_3, int col = 0) : pt_vector{pnt_1, pnt_2, pnt_3}, color(col) {};
+
+	//Find where take place other poligon from line
+        int ident_side(Plane_t<T> plane) const;
+};
+
+//Are you sure that += doesn't change rgh_pnt
+template <typename T> Point_t<T> operator+(const Point_t<T>& lft_pnt, const Point_t<T> rgh_pnt) {
+	Point_t<T> buf{lft_pnt};
+	Point_t<T>check_pnt = rgh_pnt;
+	buf.x += rgh_pnt.x;
+	buf.y += rgh_pnt.y;
+	buf.z += rgh_pnt.z;
+	assert(check_pnt.x == rgh_pnt.x && check_pnt.y == rgh_pnt.y && check_pnt.z == rgh_pnt.z);
+	return buf;
+}
+
+
+template <typename T> int Poligon_t<T>::ident_side(Plane_t<T> plane) const {
+	
+        auto it = pt_vector.begin();
+        Point_t<T> pt;
+	
+	while(true) {
+                pt = *it;
+                if((plane.A*pt.x + plane.B*pt.y + plane.C*pt.z + plane.D - tol) > 0.0) return 1;
+
+                if((plane.A*pt.x + plane.B*pt.y + plane.C*pt.z + plane.D + tol) < 0.0) return -1;
+
+                it++;
+        	assert(it != pt_vector.end());
+	}
+
+}
+
+
+//Check which side of the line the point lies
+template <typename T> int Check_side(Plane_t<T> plane, Point_t<T> pt, int right_side) {
+	int side;
+	if((plane.A*pt.x + plane.B*pt.y + plane.C*pt.z + plane.D - tol) > 0) side = 1;
+	
+	else {
+		if((plane.A*pt.x + plane.B*pt.y + plane.C*pt.z + plane.D + tol) < 0) side = -1;
+		
+		else side = 0;
+	}
+
+	if(side == 0) return 0;
+	
+	if(right_side == side) return 1;
+	
+	else return -1;
+}
+
+/*std::vector<Point_t<double>>::iterator it = triangle_1.pt_list.begin();
+while(it != triangle_1.pt_list.end()) {
+	std::cout << it -> x << ";" << it -> y << "///";
+	it++;
+}
+	
+std::cout << std::endl;*/
